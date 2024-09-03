1f90d0cc75965b8f1ee61230bb9e12e63422340f
Jensen Holm <holmj@mail.gvsu.edu>
2024-09-03 12:21:25 -0400

    computing tunnel score done!

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .headshot import get_player_headshot
-from .euclidean_dist import euclidean_distance
+from .euclidean_dist import pl_euclidean_distance
 
-__all__ = ["get_player_headshot", "euclidean_distance"]
+__all__ = ["get_player_headshot", "pl_euclidean_distance"]
--- utils/euclidean_dist.py
+++ utils/euclidean_dist.py
@@ -1,3 +1,13 @@
-def euclidean_distance(x1: float, y1: float, x2: float, y2: float) -> float:
+import polars as pl
+
+__all__ = ["pl_euclidean_distance"]
+
+
+def pl_euclidean_distance(
+    x1: pl.Expr,
+    y1: pl.Expr,
+    x2: pl.Expr,
+    y2: pl.Expr,
+) -> pl.Expr:
     """returns the euclidean distance between two points (x1, y1), (x2, y2)"""
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    return ((x1 - x2) ** 2) + ((y1 - y2) ** 2) ** 0.5
